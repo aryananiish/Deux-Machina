36d2c26d9dfc2becfd28efec215afc71c84c5e3c
Pranjal <jakharpranjal@gmail.com>
2025-12-07 23:44:24 +0530

    Submission Week0 Pranjal 250813 Modified

--- Week-0/Pranjal_250813_Week0/Lander.py
+++ Week-0/Pranjal_250813_Week0/Lander.py
@@ -22,23 +22,37 @@ total_reward = 0
 while(run):
     # this is where you would insert your strategy
     # print(x_coord, y_coord)
+
     x, y, vx, vy, angle, w, leg1, leg2 = observation
+
+    #predicted angle is the angle to which the lander will tilt after 0.7 seconds of rotation
+    #I am using predicted angle out here because when lander fires it sides engines, it over tilts so we are looking at future 0.7 seconds later
     pred_angle = angle + 0.7*w
+
+    #To move it near to 0,0 we use desired tilt.
+    #To move it, we fire the main engines. But first we tilt our lander in such a way that its nose faces the desired position, or the tilt lies in a close proximity of desired tilt and then we fire main engines
     desired_tilt = (x * 0.5) + (vx * 1.0)
-
+    
+    #To make sure that as it gets clode to surface, it must be in upright position to make a soft landing
     if y < 0.2:
-        desired_tilt = vx * 2.0
+        desired_tilt = 0
 
+    # Here I am capping my desired_tilt angle because I don't wnat it to till 90 degree even if required
     if desired_tilt > 0.4: desired_tilt = 0.4
     if desired_tilt < -0.4: desired_tilt = -0.4
 
+    
     angle_error = pred_angle - desired_tilt
     
-
+    
+    #Condition to check if the predicted angle lies within close range of desired angle
     if angle_error > 0.03:
         action = 3
     elif angle_error < -0.03:
         action = 1
+    #if it lies, then I just have to control the descent of lander.
+    # I have designed a two system check if y > 0.5, it is is allowed to descend at a much higher speed but after that threshold it must descend at a lower speed.
+    # To decrease the speed it must fire its main engines upward 
     else :
         if y > 0.5:
             if vy < -0.8:
